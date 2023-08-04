994217a8d5999b91518d2d58b11b484bab15bbba
Fede9308 <117412714+Fede9308@users.noreply.github.com>
2023-08-04 03:54:30 -0300

    first commit

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,73 @@
+package com.mindhub.homebanking.models;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    //El tipo Long representa un numero entero, pero con mas espacio de almacenamiento
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator( name = "native", strategy = "native")
+    private Long id;
+    private String dni;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public Client(){
+
+    }
+
+    public Client(String dni, String firstName, String lastName, String email) {
+        this.dni = dni;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/resources/static/manager.js
@@ -0,0 +1,55 @@
+Vue.createApp({
+    data() {
+        return {
+            email: "",
+            firstName: "",
+            lastName: "",
+            dni:"",
+            outPut: "",
+            clients: []
+        }
+    },
+    created() {
+        this.loadData();
+    },
+    methods: {
+        // load and display JSON sent by server for /clients
+        loadData() {
+            axios.get("/clients")
+                .then((response) => {
+                    // handle success
+                    this.outPut = response.data;
+                    this.clients = response.data._embedded.clients;
+                })
+                .catch((error) => {
+                    alert("Error loading clients: " + error)
+                })
+        },
+        // handler for when user clicks add client
+        addClient() {
+            if (this.email.length > 1 && this.firstName.length > 1 && this.lastName.length > 1 && this.dni.length > 1) {
+                this.postClient(this.email, this.firstName, this.lastName, this.dni);
+            }
+        },
+        // code to post a new client using AJAX
+        // on success, reload and display the updated data from the server
+        postClient(email, firstName, lastName, dni) {
+            axios.post("/clients", { "email": email, "firstName": firstName, "lastName": lastName, "dni": dni })
+                .then((response) => {
+                    // handle success
+                    this.loadData();
+                    this.clearData();
+                })
+                .catch((error) => {
+                    // handle error
+                    alert("Error to create client: " + error)
+                })
+        },
+        clearData() {
+            this.firstName = "";
+            this.lastName = "";
+            this.email = "";
+            this.dni = "";
+        }
+    }
+}).mount("#app");
