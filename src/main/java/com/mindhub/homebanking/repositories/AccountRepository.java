c46793b08147c9e531a2f08cdabde319af8d6bd6
Fede9308 <117412714+Fede9308@users.noreply.github.com>
2023-08-29 12:32:52 -0300

    First commit task8

--- src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface AccountRepository  extends JpaRepository<Account, Long> {
     boolean existsByNumber(String accountNumber);
+
+    Account findByNumber(String accountNumber);
+
 }
